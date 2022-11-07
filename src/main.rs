91f820ad959c0640bf5d126dca224fa476b341da
Crystal Kate <crystalnyan@icloud.com>
2022-11-07 16:41:01 +0500

    add mode fn from hashmap.rs to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 pub mod vec;
+mod hashmap;
 
 fn main() {
   println!("Result of the median fn on given integers: {}", vec::median());
+
+  let (integer, count) = hashmap::mode();
+  println!("Result of the mode fn on given integers (most frequent integer): {}", integer);
 }
 
 
